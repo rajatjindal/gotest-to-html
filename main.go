c36f9b33ad8f7627660e4e53736d6e05b755635e
Rajat Jindal <rajatjindal83@gmail.com>
2022-12-04 07:54:23 +0530

    more action changes

--- main.go
+++ main.go
@@ -3,28 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/rajatjindal/junit-to-html/pkg/parser"
 	"github.com/rajatjindal/junit-to-html/pkg/reporter"
 )
 
 func main() {
-	tests, err := parser.IngestFile(os.Args[1])
+	tests, err := parser.IngestFile(getInputForAction("gotest_output_file"))
 	if err != nil {
 		fmt.Println("ERROR ", err.Error())
 		os.Exit(1)
 	}
 
 	data := &reporter.TestDataWithMeta{
-		TitlePrimary:   "Spin",
-		TitleSecondary: "e2e test report",
+		TitlePrimary:   getInputForAction("title_primary"),
+		TitleSecondary: getInputForAction("title_secondary"),
 		Tests:          tests,
-		Tags: []reporter.Tag{
-			{
-				Key:   "golang",
-				Value: "1.17",
-			},
-		},
+		Tags:           getTagsFromInput(getInputForAction("tags")),
 	}
 	out, err := reporter.ToHTML(data)
 	if err != nil {
@@ -33,4 +29,30 @@ func main() {
 	}
 
 	fmt.Println(out)
+}
+
+func getInputForAction(key string) string {
+	return os.Getenv(fmt.Sprintf("INPUT_%s", strings.ToUpper(key)))
+}
+
+func getTagsFromInput(s string) []reporter.Tag {
+	tags := []reporter.Tag{}
+	if len(s) == 0 {
+		return tags
+	}
+
+	for _, pair := range strings.Split(s, " ") {
+		parts := strings.Split(pair, "=")
+
+		if len(parts) != 2 {
+			continue
+		}
+
+		tags = append(tags, reporter.Tag{
+			Key:   parts[0],
+			Value: parts[1],
+		})
+	}
+
+	return tags
 }
